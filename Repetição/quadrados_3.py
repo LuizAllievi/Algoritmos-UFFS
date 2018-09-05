dbfe5e8040985949b33235e83464a0c7a921c37d
Luiz Gustavo <luizgu2000@gmail.com>
2018-09-05 20:21:55 -0300

    Add files via upload

---
+++ Repetição/quadrados_3.py
@@ -0,0 +1,39 @@
+from turtle import *
+
+color("black")
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+
+penup()
+setpos(10, 10)
+pendown()
+
+forward(80)
+left(90)
+forward(80)
+left(90)
+forward(80)
+left(90)
+forward(80)
+left(90)
+
+penup()
+setpos(20, 20)
+pendown()
+
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+
+done()
