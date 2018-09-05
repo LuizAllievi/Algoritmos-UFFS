3fb1836510d4bd8626178427c780dde141d6a7e3
Luiz Gustavo <luizgu2000@gmail.com>
2018-09-05 20:17:33 -0300

    Add files via upload

---
+++ Repetição/triangulos_3.py
@@ -0,0 +1,37 @@
+from turtle import *
+
+color("black")
+forward(100)
+left(135)
+forward(141)
+left(135)
+forward(100)
+left(90)
+
+penup()
+setpos(8, 8)
+pendown()
+
+forward(70)
+left(135)
+forward(99)
+left(135)
+forward(70)
+left(90)
+
+penup()
+setpos(16, 16)
+pendown()
+
+forward(40)
+left(135)
+forward(56)
+left(135)
+forward(40)
+
+penup()
+setpos(0, 0)
+pendown()
+
+
+done()
